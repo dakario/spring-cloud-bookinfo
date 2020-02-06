85bef250cb506f07bd53e539422b4757980c8477
Mor Dioum <mor.dioum@beopenit.com>
2020-02-06 12:14:47 +0000

    first commit

---
+++ reviews-v1/src/test/java/com/bookinfo/ReviewsV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
