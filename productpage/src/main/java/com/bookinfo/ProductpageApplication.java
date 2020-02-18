a14d75b0cce099529cecba60ed1e3eee4c285879
modoulobolysow <msow@gainde2000.sn>
2020-02-18 13:31:21 +0000

    configuration de Hystrix et remplacement du feignclient par resTemplate

--- productpage/src/main/java/com/bookinfo/ProductpageApplication.java
+++ productpage/src/main/java/com/bookinfo/ProductpageApplication.java
@@ -1,19 +1,26 @@
 package com.bookinfo;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
-@EnableHystrix
+@EnableCircuitBreaker
 @SpringBootApplication
-@EnableFeignClients
 public class ProductpageApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProductpageApplication.class, args);
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+          return new RestTemplate();
     }
+
+	public static void main(String[] args) {
+	    SpringApplication.run(ProductpageApplication.class, args);
+	}
 
 }
--- reviews/src/main/java/com/bookinfo/entity/Reviews.java
+++ reviews/src/main/java/com/bookinfo/entity/Reviews.java
@@ -1,19 +1,94 @@
 package com.bookinfo.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.io.Serializable;
 
-@Builder
-@Getter
-@Setter
+
 public class Reviews implements Serializable {
-    private int idProduct;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private int idProduct;
     private int idReviewer;
     private String text;
     private String reviewer;
     private Object rating;
     private String color;
+    
+    public Reviews idProduct(int idProduct) {
+    	this.idProduct = idProduct;
+    	return this;
+    }
+    public Reviews idReviewer(int idReviewer) {
+    	this.idReviewer = idReviewer;
+    	return this;
+    }
+
+    public Reviews text(String text) {
+    	this.text = text;
+    	return this;
+    }
+
+    public Reviews reviewer(String reviewer) {
+    	this.reviewer = reviewer;
+    	return this;
+    }
+    public Reviews rating(Object rating) {
+    	this.rating = rating;
+    	return this;
+    }
+    public Reviews color(String color) {
+    	this.color = color;
+    	return this;
+    }
+    
+    public int getIdReviewer() {
+    	return idReviewer;
+    }
+    
+    public void setRating(Object rating) {
+    	this.rating = rating;
+    }
+
+	public int getIdProduct() {
+		return idProduct;
+	}
+
+	public void setIdProduct(int idProduct) {
+		this.idProduct = idProduct;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getReviewer() {
+		return reviewer;
+	}
+
+	public void setReviewer(String reviewer) {
+		this.reviewer = reviewer;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Object getRating() {
+		return rating;
+	}
+
+	public void setIdReviewer(int idReviewer) {
+		this.idReviewer = idReviewer;
+	}
+    
 }
